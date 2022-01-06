054b78789167ec65a7a8e99cb7df343eb22c4238
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-01-06 17:18:06 -0500

    Update Apirequest.js

--- nftrarity/src/components/Apirequest.js
+++ nftrarity/src/components/Apirequest.js
@@ -1,28 +1,53 @@
 import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
+import DataGather from "./DataGather";
 
 class Apirequest extends React.Component {
-  state = { contractAddress: "" };
+  state = { contractAddress: "", processed: 0 };
+  afterSetStateFinished = async () => {
+    for (let i = 0; i < 10001; i += 50) {
+      const collectionInfo = await axios.get(
+        "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=" +
+          i +
+          "&limit=50&asset_contract_address=" +
+          this.state.contractAddress,
+        {}
+      );
+      this.setState({ processed: i });
+      //TODO, map response and find rarity values
+      console.log(collectionInfo.data.assets);
+    }
+  };
   onSearchSubmit = async (term) => {
     const response = await axios.get(
       "https://api.opensea.io/api/v1/collection/" + term,
       {}
     );
-    this.setState({
-      contractAddress:
-        response.data.collection.primary_asset_contracts[0].address,
-    });
+    this.setState(
+      {
+        contractAddress:
+          response.data.collection.primary_asset_contracts[0].address,
+      },
+      () => {
+        this.afterSetStateFinished();
+      }
+    );
+  };
+
+  AddressFound = () => {
+    console.log(this.state.contractAddress);
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        {this.state.contractAddress}
+        Processed: {this.state.processed}
       </div>
     );
   }
 }
 
 export default Apirequest;
+// <DataGather contractAddress={this.state.contractAddress} />
