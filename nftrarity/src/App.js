760b54d0a18ebcffe7dba569886a43602f08326d
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-01-06 13:19:49 -0500

    Create App.js

---
+++ nftrarity/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Apirequest from "./components/Apirequest";
+
+function App() {
+  return (
+    <div>
+      <Apirequest />
+    </div>
+  );
+}
+
+export default App;
